c6527def154ae206f8f0934503906a2ae5823167
Dominique Chin-Jones <djones@rapptrlabs.com>
2024-12-06 23:56:33 -0500

    hello world

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Branch
 
-under construction!
+Under construction!
+
+Looking to enhance digital commmunication
+
+- [ ] Send messages in a single channel
+- [ ] Send messages to multiple channels
+- [ ] Send messages to a specific channel with a specific user
+- [ ] Enable users to subscribe to email threads...
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+    let port = "4000".to_string();
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", port))
+        .await
+        .expect("Failed to bind to port!");
+    println!("App started on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start server!");
+}
+
+async fn hello_world() -> impl IntoResponse {
+    "Hello, World!"
 }
